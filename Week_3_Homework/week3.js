12d1aaf0db318856eed92d8243acb586b361a344
Anthony Tuzzo <70153851+Anthony-Tuzzo@users.noreply.github.com>
2020-09-15 14:19:34 -0500

    Homework update

--- Week_3_Homework/week3.js
+++ Week_3_Homework/week3.js
@@ -1,2 +1,23 @@
-let pizza=("pizzayum");
-const yum=("pizza")
+let pizzaToppings = ['Pepperoni', ' Sausage', ' Pineapple', ' Ham', ' Olives'];
+let pizzaSize = [' Small', ' Medium', ' Large', ' X-Large'];
+let pizzaCrust = [' NY Style ', ' Chicago Style ', ' STL Style '];
+let greeting = "Benvenuti a la Pizzeria Tuzzo!";
+function greetCustomer() {
+    return (`${greeting} The available toppings today are ${pizzaToppings}`)
+};
+console.log(greetCustomer());
+function getPizzaOrder() {
+  return `One ${myPizza.size[1]} ${myPizza.crust[0]} crust pizza with ${myPizza.toppings[1]}, ${myPizza.toppings[2]}, and ${myPizza.toppings[4]} coming up!`
+};
+function getPizzaOrder() {
+    return ("One" + pizzaSize[3] + pizzaCrust[0] + "pizza with " + pizzaToppings + " coming up!")
+};
+console.log(getPizzaOrder())
+function preparePizza() {
+    return ("Cooking" + pizzaSize[3] + pizzaCrust[0] + "pizza with " + pizzaToppings)
+};
+console.log(preparePizza())
+function servePizza() {
+    return ("Order up! Here's your" + pizzaSize[3] + pizzaCrust[0] + pizzaToppings + " Enjoy!")
+};
+console.log(servePizza())

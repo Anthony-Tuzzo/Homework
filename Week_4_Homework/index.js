ffd0bdb6b1f11165a2465b07f1ab5d5c40067ce5
Anthony Tuzzo <70153851+Anthony-Tuzzo@users.noreply.github.com>
2020-09-16 16:20:23 -0500

    Updated Week4 Assignment

---
+++ Week_4_Homework/index.js
@@ -0,0 +1,10 @@
+const app = document.getElementById('root')
+const myList = `
+  <ul>
+    <li>Domestic League</li>
+    <li>Domestic Cup Tournament</li>
+    <li>Hattrick Masters League</li>
+    <li>National Team World Cup</li>
+  </ul>
+`
+app.innerHTML = myList
